2ab3eb480331986e13f8781f4f9f6295eb1d031b
CrisLeash <pinipon.cr@gmail.com>
2021-10-28 00:16:31 +0200

    fixing positioning

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-// Todo: 1- change div opacity
-// todo: 2- img ease-in on Load
+// 1- select the portfolio button
+//2- add eventlistener click
+//3- make it move when click
+//4-open the link in new tab
+
+//BACKGROUND
+//1- make some shape in the background
+//2- make the background move when scrolling
 
