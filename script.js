d92f610a31d837f0cbfb32fe15adaf123ee3d2d7
Tom Lukman <tolukman@hotmail.com>
2021-10-31 10:17:34 +0100

    Initial quasar commit.

--- script.js
+++
@@ -1,9 +0,0 @@
-// 1- select the portfolio button
-//2- add eventlistener click
-//3- make it move when click
-//4-open the link in new tab
-
-//BACKGROUND
-//1- make some shape in the background
-//2- make the background move when scrolling
-
---
+++ src/pages/Index.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-page class="q-pa-lg">
+    <div class="column items-center">
+      <q-avatar class="shadow-5" size="300px">
+        <img src="~assets/cv-foto.jpg">
+      </q-avatar>
+      <div class="text-h1 text-weight-medium q-mt-xl">Hi, I'm Cris.</div>
+      <div class="text-h5 text-weight-medium q-mt-sm">Welcome to my first Landing Page, built from scratch.</div>
+      <div class="text-body1 text-italic q-mt-sm">(I'm still learning)</div>
+      <q-btn class="q-mt-md" color="primary"  href="https://crodrigues.portfoliobox.net/" label="Click me" icon="touch_app" size="lg" target="_blank" type="a" />
+      <q-img
+      class="rounded-borders q-mt-lg"
+      src="~assets/background-image.jpg"
+      spinner-color="white"
+      style="max-width: 800px"
+    >
+      <div class="absolute-bottom text-subtitle1 text-center">
+        Thanks <a id="foto credits" href="https://www.instagram.com/nace.grg/" target="_blank">Nace</a> for the photo.
+      </div>
+    </q-img>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+  methods: {
+    shittyCris(cris) {
+      if (cris === true) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    betterCris(cris) {
+      return cris;
+    }
+  }
+})
+</script>
