04d1039fab6d14dc25a56aa0729a4a8aaf5d3ce0
CrisLeash <pinipon.cr@gmail.com>
2021-10-31 23:54:17 +0100

    Changed content

--- script.js
+++ script.js
@@ -7,3 +7,9 @@
 //1- make some shape in the background
 //2- make the background move when scrolling
 
+const btnPortfolio = document.getElementById("btn");
+
+btnPortfolio.onClick = (event) => {
+    event.preventDefault();
+    console.log("hello");
+}
