38b5467fadc4d969cc5380b74d098e0889aa0dfa
CrisLeash <pinipon.cr@gmail.com>
2023-08-22 21:23:33 +0200

    changed text

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 //3- make it move when click
 //4-open the link in new tab
 
-//BACKGROUND
+//BACKGROUND - css
 //1- make some shape in the background
 //2- make the background move when scrolling
 
