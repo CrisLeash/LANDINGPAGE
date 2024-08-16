40133a1d5e58aabf2077873e93258ec324a7deb6
Your Name <crisleash@pm.me>
2024-08-16 15:16:37 +0200

    added GSAP

---
+++ my_script.js
@@ -0,0 +1,8 @@
+import { gasp } from "gasp";
+
+const circleAnimate = document.getElementById('circleAnimation') 
+
+window.addEventListener('scroll', function() {
+circleAnimate.getElementsByClassName
+  });
+  
