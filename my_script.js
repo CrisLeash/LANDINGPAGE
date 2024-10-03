67edc73ae2b0d3ff8e6a2737b4ff605e98a20cdb
Your Name <crisleash@pm.me>
2024-10-03 16:59:50 +0200

    buttons are now working

--- my_script.js
+++ my_script.js
@@ -14,3 +14,23 @@ window.addEventListener('scroll', function() {
   window.addEventListener('load', () => {
     ScrollTrigger.refresh();
 });
+
+document.getElementById('btn_portfolio').addEventListener('click', function() {
+  window.open('https://crodrigues.portfoliobox.net/', '_blank');
+  return false;
+});
+
+document.getElementById('btn_linkedin').addEventListener('click', function() {
+  window.open('https://www.linkedin.com/in/crodrigues93/', '_blank');
+  return false;
+});
+
+document.getElementById('btn_twitter').addEventListener('click', function() {
+  window.open('https://twitter.com/cris_Leash', '_blank');
+  return false;
+});
+
+document.getElementById('btn_gitHub').addEventListener('click', function() {
+  window.open('https://github.com/CrisLeash', '_blank');
+  return false;
+});
